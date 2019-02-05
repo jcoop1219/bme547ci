12b4023be4c20bce7807928af00209ac85e687a1
Jason Cooper <jason.cooper.12@gmail.com>
2019-02-05 01:03:15 -0500

    Adding basic test case to test_tachycardia.py and creating Travis CI .yml file

--- test_tachycardia.py
+++ test_tachycardia.py
@@ -1,5 +1,7 @@
 def test_is_tachycardic():
     from tachycardia import is_tachycardic
 
-    result = is_tachycardic("134dwqtachycardic13445")
-    assert result == True
+    stringToCheck = "tachycardic"
+    result = is_tachycardic(stringToCheck)
+
+    assert result is True
