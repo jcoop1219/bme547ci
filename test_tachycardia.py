2435ba5db5a7e55219879e4119f317881f820e77
Jason Cooper <jason.cooper.12@gmail.com>
2019-02-05 01:42:38 -0500

    Adding every test case for direct-matching testing in test_tachycardia.py

--- test_tachycardia.py
+++ test_tachycardia.py
@@ -1,10 +1,28 @@
-def test_is_tachycardic():
-    from tachycardia import is_tachycardic
+import pytest
+from tachycardia import is_tachycardic
 
-    stringToCheck = "tachycardic"
-    result = is_tachycardic(stringToCheck)
-    assert result is True
 
-    stringToCheck = "tachycardic"
+@pytest.mark.parametrize("stringToCheck, expected", [
+                        ("tachycardic", True),
+                        ("tachycardiC", True),
+                        ("TachycardiC", True),
+                        ("TACHYCARDIC", True),
+                        ("tachycardiC", True),
+                        (" tachycardic", True),
+                        ("tachycardic ", True),
+                        ("tachycardic!!!", True),
+                        ("!!!tachycardic ", True),
+                        ("!!!tachycardic!!!", True),
+                        ("tachycardic3", True),
+                        ("3tachycardic", True),
+                        ("Ztachycardic", True),
+                        ("tachycardicZ", True),
+                        ("tachy3cardic", False),
+                        ("tachyZcardic", False),
+                        ("3achycardic", False),
+                        ("tachycardi3", False),
+                        ("tachy cardic", False),
+                        ])
+def test_is_tachycardic(stringToCheck, expected):
     result = is_tachycardic(stringToCheck)
-    assert result is True
+    assert result is expected
