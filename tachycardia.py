027e4aaa885be7d5088b7cf5d7eca0af775328d0
Jason Cooper <jason.cooper.12@gmail.com>
2019-02-06 02:15:32 -0500

    Updating logic for fuzzy matching to use begin and end range of letters to check

--- tachycardia.py
+++ tachycardia.py
@@ -4,13 +4,18 @@ def is_tachycardic(stringToCheck):
     # Initialize variables
     testString = "" # string which will be built out in loop below
     typoCount = 0 # number of typos (uppercase excluded) in stringToCheck
+    beginRange = 0
+    endRange = 1
 
     convertedString = stringToCheck.lower()
     for char in targetString:
         testString = testString + char
-        if testString not in convertedString:
+        if testString not in convertedString[beginRange:endRange]:
             typoCount += 1
             testString = ""
+        else:
+            endRange += 1
+
     if typoCount <= 2: # maximum tolerance is 2 missing or misspelled letters
         return True
     else:
