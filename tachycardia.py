f73a29a6e225c1cdfc337b216cd813fd32ec7e06
Jason Cooper <jason.cooper.12@gmail.com>
2019-02-05 23:23:47 -0500

    Updated is_tachycardic to do hazy/fuzzy matching on tachycardic so it can tolerate max of 2 typos

--- tachycardia.py
+++ tachycardia.py
@@ -1,6 +1,17 @@
 def is_tachycardic(stringToCheck):
+    targetString = "tachycardic" # string to check against
+
+    # Initialize variables
+    testString = "" # string which will be built out in loop below
+    typoCount = 0 # number of typos (uppercase excluded) in stringToCheck
+
     convertedString = stringToCheck.lower()
-    if "tachycardic" in convertedString:
+    for char in targetString:
+        testString = testString + char
+        if testString not in enteredString:
+            typoCount += 1
+            testString = ""
+    if typoCount <= 2: # maximum tolerance is 2 missing or misspelled letters
         return True
     else:
         return False
